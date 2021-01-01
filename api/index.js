3ca18dba1e6b2c81d2fc7457e3655500b74f947f
Pavan Jadhaw <pavanjadhaw96@gmail.com>
2021-01-01 14:30:10 +0530

    feat: adapt to vercel serverless functions

--- api/index.js
+++ api/index.js
@@ -1,5 +1,4 @@
 const { parse } = require('url')
-const { send } = require('micro')
 const got = require('got');
 const cache = require('memory-cache')
 
@@ -28,10 +27,10 @@ module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
 
   const { query: { url } } = parse(req.url, true)
-  if (!url) return send(res, 401, { message: 'Please supply an URL to be scraped in the url query parameter.' })
+  if (!url) return res.status(401).send({ message: 'Please supply an URL to be scraped in the url query parameter.' })
 
   const cachedResult = cache.get(url)
-  if (cachedResult) return send(res, 200, cachedResult)
+  if (cachedResult) return res.status(200).send(cachedResult)
 
   let statusCode, data
   try {
@@ -44,7 +43,7 @@ module.exports = async (req, res) => {
     data = { message: `Scraping the open graph data from "${url}" failed.`, suggestion: 'Make sure your URL is correct and the webpage has open graph data, meta tags or twitter card data.' }
   }
 
-  send(res, statusCode, data)
+  res.status(statusCode).send(data)
   // Cache results for 24 hours
   cache.put(url, data, TWENTY_FOUR_HOURS)
 }
